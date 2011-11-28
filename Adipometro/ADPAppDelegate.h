0ec5991180013a18c851da7ebba901590bf10fa0
tercio <tercio@mpc.com.br>
2011-11-28 14:16:14 -0200

    Initial Commit

---
+++ Adipometro/ADPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADPAppDelegate.h
+//  Adipometro
+//
+//  Created by Tercio Esperandio on 11/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ADPAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Adipometro/ADPAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  ADPAppDelegate.m
+//  Adipometro
+//
+//  Created by Tercio Esperandio on 11/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ADPAppDelegate.h"
+
+@implementation ADPAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
